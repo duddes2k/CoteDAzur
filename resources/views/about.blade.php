aaa253d2cd11c64f037eae061eedb7be00f55ca6
Duddes <duddesatwork@gmail.com>
2019-03-04 21:06:34 +0800

    Update: update about page text

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -56,7 +56,7 @@
                     <div class="section-title  text-center">
                         <h2>About us</h2>
                         <span class="title-line"><i class="fa fa-ship"></i></span>
-                        <p>We are located in Cannes, France, the ultimate glamorous destination in The French Riviera with its Luxury Villas, Cars and Yachts and known for the famous Cannes Film Festival.</p>
+                        <p>Located in the city where the ocean view is as spectacular as the film festival, Cannes is just one of the glamorous cities to be explored in Côte d’Azur or the French Riviera. Home to Côte d’Azur Luxury Rentals, this city has more to offer as well as nearby places.</p>
                     </div>
                 </div>
                 <!-- Section Title End -->
@@ -69,15 +69,20 @@
                         <div class="display-table-cell">
                             <div class="about-content">
                         
-                                <p>We offer: 
+                                <!-- <p>We offer: 
                                     <ul class="ml-4">
                                         <li><i class="fa fa-angle-double-right"></i> A Spectacular Super Yacht, 31-meters long, to explore the beautiful French Riviera, Italy, Spain, Corsica, etc.</li>
                                         <li><i class="fa fa-angle-double-right"></i> A beautiful Rolls-Royce Phantom Drophead to drive around the city and breathtaking coastline of the French Riviera.</li>
                                         <li><i class="fa fa-angle-double-right"></i> A super-fast and luxurious VanDutch 40 Speedboat to enjoy the seabreeze and amazing beaches of the French Riviera.</li>
                                     </ul>
-                                </p>
+                                </p> -->
 
-                                <p>Cannes has a small airport but you may prefer to fly into Nice Airport, rent our luxury rolls royce car and drive into Cannes with the top down.</p>
+                                <p>Explore Côte d’Azur and bask in the sun in our spectacular 31-meter long Mega Yacht spacious enough for parties with friends or a family trip.</p>
+
+                                <p>Need a faster way to explore the beaches and crystal blue waters? Our superfast and luxurious VanDutch 40 Speedboat is the top choice.</p>
+
+                                <p>If you wish to have a look around the city and the coastline by land, our chauffeur-driven grand Rolls-Royce Phantom Drophead can take you around comfortably. Our ultra-comfortable Rolls-Royce can also pick you up from Nice Airport if you wish to alight there instead of flying into Cannes’ small airport.</p>
+                                
                                 <div class="about-btn">
                                     <a class="sliding-link" href="#slideslow-bg">Book Now</a>
                                     <a class="sliding-link" href="#footer-area">Contact Us</a>
