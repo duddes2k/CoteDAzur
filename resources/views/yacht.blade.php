53cfd287ebf1b7584b218ef896e8dc2bbf30d56a
Duddes <duddesatwork@gmail.com>
2019-04-11 11:45:42 +0800

    Update: added new yacht photos

--- resources/views/yacht.blade.php
+++ resources/views/yacht.blade.php
@@ -12,14 +12,55 @@
                     <div class="car-details-content">
                         <h2>LUXURY MEGA YACHT</h2>
                         <div class="car-preview-crousel">
+                           
                             <div class="single-car-preview">
-                                <img src="https://l2belg.ch.files.1drv.com/y4ml26vKPn8L_nmInRpzG94PWaTbReVx02Ce5uKkjxOJn44gWK8WUVNoPVAABbddmRUjmtnIapNgSOEQY7bCajMXi-XmAd4aSPL_5U27C7B5bhai2sLxJ91fHl-yUXZDfw7Gk55KzgF0icd8G-iuuNZ7T3zwAd1GgccpXkb4143QGmqGQQUOWk5sTnHvDkzdPDBQvVz57NP39RsWzO2jlz85w?width=1233&height=868&cropmode=none" alt="JSOFT">
+                                <img src="https://v7tfka.ch.files.1drv.com/y4mmAYc6P56fsqtU2c7AEhxI2LH9bpujIbPg0PG39VOMSFWirZ1I4pNmI0Y6CEilYkV17Lr2TE8wLHg_0MBPWMSnWzYJyT0E037Ddc3G-jnp1jVGMsZ96YDpB2tjRdCwLheBbBy4hPJ1JLn32cTIYEnnGriJTcCirhhKvw6jttaySyqKHyNRfEnjdgkTbsmYZsdZqmhhOo2gTDzglTThX9AqA?width=7360&height=4912&cropmode=none" alt="Yacht"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://qosbfw.ch.files.1drv.com/y4mS7yqIit-a98T5oRrRVX9NNMq1X_M9hHl88ECO8fU2EIVbHVt5cU-MQulACf0XKTmpbc7nlp2rbDxAryeztaGKgOOJMUCIlhSM4wPDY6QwoRQf-KbT1s_kg5-Q3_5ouFfu-bQt150ymyLn11eX8sbNzsnuzueFy_0mr4iIUin3zjKG9sFlbu91NjVIMiqViQDETNEWa1LyZYl-Tj7wOquog?width=1227&height=869&cropmode=none" alt="JSOFT">
+                                <img src="https://uhrmbq.ch.files.1drv.com/y4m3cNspJlrBd6jN36HLqlamuHKG-1i6Q7YJ8QarsSnKaMZWnWYVHEjfBjgA6RzRW6oLSuodbVCClSSFyrrniQqdzgQD1Q3azXRvu9YqmDRPPgUl_sABcQkM2Ux_KX7Q5K6_jzz87WvMDCkxO-4aAPiVL-ukuOlcxINTSqXaaOMj3MKc5dXZAgpJM31ECyjdJ16_JGIzZQEi35uZpLgwna0kA?width=7360&height=4912&cropmode=none" alt="Yacht"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://l2belg.ch.files.1drv.com/y4ml26vKPn8L_nmInRpzG94PWaTbReVx02Ce5uKkjxOJn44gWK8WUVNoPVAABbddmRUjmtnIapNgSOEQY7bCajMXi-XmAd4aSPL_5U27C7B5bhai2sLxJ91fHl-yUXZDfw7Gk55KzgF0icd8G-iuuNZ7T3zwAd1GgccpXkb4143QGmqGQQUOWk5sTnHvDkzdPDBQvVz57NP39RsWzO2jlz85w?width=1233&height=868&cropmode=none" alt="Yacht">
+                            </div>
+                             <div class="single-car-preview">
+                                <img src="https://qosxfw.ch.files.1drv.com/y4mMsDCSo9LDEutvVzfZcboJcxB2CPFrEWXw-dIOfjULFu1GyQ6-ec_rE2_Xc_hrZ5ZAsLjRLQfAZvbCbcB1blCbI734TFfq91gxB4vNPoguVNogYurzr7-NpntXaCtCrEJyGjU9ECmVd3NP-EacWMKRcYyUsee_3IxUO24MTQaZw6Mxxyww92gXtsSNPqpqRnU2xs1-aDdcZ_X0vGhKcFnAw?width=5724&height=3816&cropmode=none" alt="Deck"/>
+                            </div>
+                            <div class="single-car-preview">
+                               <img src="https://jefnjw.ch.files.1drv.com/y4maIU0_nNYiAszRKJL058aGlaYK8bV-LblT2IKJXl2ZqOu65NMBemoLwzdFNPSjWIbYU2x8ziuebAXaXI7I2R9dLr86zK3eDxl_eNGtDmgJSHVNOHVuuRfEWoFEFRMxwJv7lUpFs65094mwNiQnvwTAOfjGz4-BF8AONKh8qri0Fusi8xdvZ5qaE_SrYX27wm8T4MSYsR_VSTMGm8B0LDPkQ?width=6000&height=4002&cropmode=none" alt="Deck"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://jecdeq.ch.files.1drv.com/y4mMr9TQW0xNHK_THwjaqWIGDOaR7nezSgQPYhCkIoSukwq2vgAxRKZTaIkp0L4NFaWEMPsbk9L7VStF-_V1TTocQ8XXoVjlxyu9s2oQ_CORw-cY_eZtKwGXn_r90FES-h7BqtFIoFMG_mPM5lxlIafT5YUyP5HqCTmm-fhtwWkJPONAzHObs_meY2Qdr2TfxOVgmMuch_yYsazxlM25Yyv_A?width=6000&height=4002&cropmode=none" alt="Deck"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://qosbfw.ch.files.1drv.com/y4mS7yqIit-a98T5oRrRVX9NNMq1X_M9hHl88ECO8fU2EIVbHVt5cU-MQulACf0XKTmpbc7nlp2rbDxAryeztaGKgOOJMUCIlhSM4wPDY6QwoRQf-KbT1s_kg5-Q3_5ouFfu-bQt150ymyLn11eX8sbNzsnuzueFy_0mr4iIUin3zjKG9sFlbu91NjVIMiqViQDETNEWa1LyZYl-Tj7wOquog?width=1227&height=869&cropmode=none" alt="Deck">
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://idi0xw.ch.files.1drv.com/y4mvCfub1M6OAtDCdgVF6jV2svGqgQAgjkLu01PEWU2qycFrBZUf9quCn8pfij1Dudku3MqEP626geagEMMPzdRG7Oco6XgZLXAwn5wgyQqvs_xFKUBgIOu8Nuh_RcPlC31mkvxB3pnLRdKZEuuuLswk2CheAgi0P3801jXuShXu-Z1J-OXlXCKSRufvvy3VbqigeS6Gyg75cSLdEFhLWINig?width=7360&height=4912&cropmode=none" alt="Deck"/>
+                            </div>
+                            <!-- Interior -->
+                            <div class="single-car-preview">
+                                <img src="https://uhtneq.ch.files.1drv.com/y4m7tcg57T3ZxFgTii-5zWAkKAK8CKhhcUaKnbfx1WHJItZCpp1qLRD1Xh8jHP0LDq-Akjvo8XGQpyvwfWY_EUnO_AycCp8M7ePqJ9CMJ2w7baMwEaBPZM1sQLpX8dohPhSQ-8aJwcDIbqjI1W9YzQ9ktqvxi2Zg-L425CAYEHr7xBzpKt6w6J44tLL41HVQVyruBcL2MclgN5_9V3-BYXV6w?width=7362&height=4916&cropmode=none" alt="Interior"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://uhsc7g.ch.files.1drv.com/y4mPMYBIYuH52zXIVN5Z8X8D1YWaQ6HzUosjgXXWsfJI7XtXO2BGkug3uK6WnFFWHF0YbKld7IZc0lOTRclMSXyjPWK3FCNtOv4nd-F9LSjYAgzeus-yNkcaFvOhovfQ89m7omoGsRx2tlvp2_tmIztA2Nik1MXcgMBv9y0VdkDVn8lCAmGmYfRj8W-U_KAR9rQG0QiEFQH3vjGyFzdt6tZnQ?width=7363&height=4914&cropmode=none" alt="Interior"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://l2bova.ch.files.1drv.com/y4mVh97UMwZwhUaC1rcB_uf8M0loZuLSFrTcV8qfyqHdKh-Dzpudc9XnyrvHt9y5WYzm4fYoEcAVqI4jX2S5S_iUvHtsTOdEkNM5U7SYGTrEW9NXhs_jFLTo_L1z5ZkBxy_bZJvCDN6wzjVmtjAWsFS-xTEqJJd-SYmYHAin6Li96eQhKj2_w8XEPOsz58v30rcpZfY_iyLKJzuRaHLg97tww?width=7360&height=4912&cropmode=none" alt="Interior"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://l2cxxg.ch.files.1drv.com/y4m7Ww4AJV2Jubu060AlV8PCh-yObPy1U1T-YjMdGgETB8R-JJDKplVx9Xxjj_D9TT4qY8tJxcm2O1G0HsZFMyXjAoQyVXQqAnPy3vAxKosvSy9onx0NCKDV__rZV9BTBTmga5Bm0TuSlj_oOrNyEczlIpTNiOWcdalqKc9VQYBWjtFBus0NLCqibQCM4Q3MuqLhHJNvt33Zg6GomQda4RVCA?width=7360&height=4912&cropmode=none" alt="Interior"/>
                             </div>
                             <div class="single-car-preview">
                                 <img src="https://qosppq.ch.files.1drv.com/y4mNCyHrEiWzoRWCE4wHx3WwGWtXMqawnmGTGSBizvMha2jsg5tulVaGlPU9VIQ9tppUFj3NOeOa9DnVSfnaocueDcgckFO0Z9kPil36GmentYUhIBltZCJOhuXCuOFUJmQxnpumvU5eczp3SBWV-qd14Dr_S0FYZu0RXPZ-n8ZwMHS4waK_vdgX3ub-s0m5nctJRxGFToZ6nm1dbvGG7_i8A?width=1235&height=869&cropmode=none" alt="JSOFT">
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://qovvuw.ch.files.1drv.com/y4mbw6d40K-DpT-mQZ4UOe_DcLOMG1dczX6DuHBaqSgNb9hpQgArVZA0vcj7QqpaAHYS4hlIWWeBtS9FMJxkFGsZ41Jh4B0iu7VCvcHkU0sTSApjUKE-0WH4opPULrIaffiVMoQ8vb7AVbHs1Q-MfRHDIqr4eRhiMTvjHvYejCCZytf5uwNnriuz_odovTQb7yNDYhNre2RaGOsmYQSiZKgxw?width=7360&height=4912&cropmode=none" alt="Interior"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://l2cepq.ch.files.1drv.com/y4muCdpnDTH-1Kh92WhFQQyzIP2Yeejdjx2uGMjP5ZPyru9rRhSxnxxJvOgODVnJ_lwheeVvuW4nBT-VbwSylZQ2KGj8erKC9vRu38devVyySg6j8-rjbqPBxM7AZ9Rg4j7wbuq9SAvsK1TVZbTXMQFwrbvIRAeHjHus_rQzDPHMKfmasFlUpbj0GeZ5Vr7zBr7Syr4bcn90B6qmemLAtCbEQ?width=7111&height=4743&cropmode=none" alt="Interior"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://qoukma.ch.files.1drv.com/y4mU2c68Rq7S0bn6wVKupqVBLgcR4CedR6s2Xlt1H0W_K0sVF0ubSM1naN5HZprCPhOg5QZG11G52nrm5NZ1vrifMIRbUj3uMGjZRzmX5EQSQSqVEHBvlFHfNkAoWp6q-WmnW4wXvJpWG3UcAFnFHcwwqb3DeoNPiaUdNny4_OjBRU3P3vK6d69OpCiJS43cpTE7QxjCT6ejY1omIrm98TuQQ?width=7360&height=4912&cropmode=none" alt="Interior"/>
                             </div>
                             <div class="single-car-preview">
                                 <img src="https://qosw7a.ch.files.1drv.com/y4mMZ564BK1DRBop1AdXytNgy6XD6A4DEObw7hvzMkT0AdCwEc4VjwizkyJ2lWVeTj1q-Po19RV7mfnSw8bbBwBLGy7jthmGQo4bBVGsFwDwpRPwXRTmDClO6_EJVPkK0_akNoUK5HHocwf_9bwe6p-zahSEvSNmsJ1nPQ5uqV2dbzGlKx0z_iAFq052PjRAuLtrY-M-BaFqkYFpOu_a_WhcA?width=1231&height=865&cropmode=none" alt="JSOFT">
@@ -31,6 +72,10 @@
                                 <img src="https://l2dduq.ch.files.1drv.com/y4mYoj24im1p9_HMO5fS5xc8Q11SpVY3kOevZkBxGSgvUyau3b93wt1RQNdV6aSVn4pRuLuDESnfoRur5px9yYY56fzblsLPyjC1cdWNy2ZJSRvXPn3T1uduNRVKytVLjITu125d_JO3dF_Lp9B1ZQNW8NMHVir_2fmtyhRTxsX2j1OzR4hqjtB1Y0pz3YR8qECAqW7qbvpqTOoAUe1zKJ14Q?width=1230&height=870&cropmode=none" alt="JSOFT">
                             </div>
                             <div class="single-car-preview">
+                                <img src="https://jed0dg.ch.files.1drv.com/y4mswzOf2HScHKvZOihvtQuujnzhOUMvWzy9UFMqfTbneml_n5fB88cfB4E35gsGTZookQ9zOw62LSFP61bbDPkHh7qDOI4n_BWrSHsYXqadc7BH1m81fMAkDeGB0gQrD0XXq4SiRDvT20YEYDLNmngeOe5Lsj7ssdHlrPFsTYBOatezlbDUNHz5ZDokNxjZQt7risDXtxTm1S7YeqIlNYWnQ?width=7952&height=5304&cropmode=none" alt="Interior"/>
+                            </div>
+
+                            <div class="single-car-preview">
                                 <img src="https://qouoma.ch.files.1drv.com/y4mwciIzo23Df32zPgsvjf1uR-rNfQoaqfawjFoFb33DahznhBUPA8gzh8ePuhkK-zeWgf235FiF58jE-jwISnqTPdQi8ZWnj_xUcyoVhiMNR1Fc7drvViHfUWUM6dZITILnXUzDm49CzpLDJcgA57xy6swnyobKsV2VXvLg_ONT2ZyRypGa0EL7g1wWNjgaRZMPJMcVyWkeiIVVnaL8sj_Zw?width=1232&height=870&cropmode=none" alt="JSOFT">
                             </div>
                             <div class="single-car-preview">
@@ -41,6 +86,15 @@
                             </div>
                             <div class="single-car-preview">
                                 <img src="https://qouvdw.ch.files.1drv.com/y4mD47N5HRQeTUMtxhSrdy9bV4qf3SA6MB6IK4z_q4Nv4F8SxekgqrnQ08dnqmCz8yKWwHX_K9nbIDyHjQ3JXhI0clh71Pcgms_5lbqa3A4e8TYYa9JKa6lJ9UQ0ZC4Uqy0xAIyMg1BdbCYqZw-M2vU4GgaLWtVyAJrrC9ccy5oNAgmmX4gAqG3B2OCzNouoplb91L8mjyofwdxggdpAUznNw?width=1231&height=872&cropmode=none" alt="JSOFT">
+                            </div>
+                            <div class="signal-car-preview">
+                                <img src="https://7l2nva.ch.files.1drv.com/y4mXGbxKWqISCrjCA8BK4m1Fx4kXYMma6wpyJTjOrsu3E57X4vo5CilT3etNjMnk3f1lhqLZIwW9UJgy1l_t49fjwdc3qd9VaWPpzgT_LVN7TRnLRlsIWnMkTIWidDjvTb2JtIh4lFBM0NC88f8XT2qvt2xwHANUocgWcLMDDK9wwKY9TmMIbKcEcKVaO1ebNi5iW8GAyydPPZ7k0ZUicumig?width=6588&height=4405&cropmode=none" alt="Interior"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://7l0omq.ch.files.1drv.com/y4m5GtHQk8aSz5cbEZSVWxSTHuXCTNTByR-9zOY-vkZdWEdxafoD_sx3zLhjb0BYIMmJh5ip59BoLkl9gLTDHRO3bCFh1jlDPe1aLHpkapBAf1FW_cR2Ohi7JvLlY9nMKZeUTzJ-sQRewyCMuZGPEcD4FTBKqdvCk2VN4dJaDvEitsQaJg9otLpTcfM3yUWtXwjmRbVZmcVs_rwwi83t78BVQ?width=7364&height=4918&cropmode=none" alt="Interior"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://qov2ag.ch.files.1drv.com/y4mDcklrCSQfFRKZNswZE5wA-16JYC1jD6TeeU4z_dMJQfdeBPp_PuaULTSkjcvlIcYSGxhxsl6LwG3Jmoq5u54z3ESlRvuEbyub6-R21BR3y1VJN8MwEe10BXYO46QLvVV2K4_53TJvPn69tjoNsaQTiIHLaeUIcojaOkNz47XaLzL_LeH7oVUv3aJL5uA_WASuqn3fll2veiT7W2EvqsSow?width=6000&height=4000&cropmode=none" alt="boat"/>
                             </div>
                         </div>
                         <div class="car-details-info">
