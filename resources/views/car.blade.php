1f105e32abb8926209adeaa7297da3c8d6f7615a
Duddes <duddesatwork@gmail.com>
2019-04-16 14:44:24 +0800

    Update: replaced car photos with edited photos from John

--- resources/views/car.blade.php
+++ resources/views/car.blade.php
@@ -13,61 +13,82 @@
                         <h2>Rolls-Royce Phantom Drophead</h2>
                         <div class="car-preview-crousel">
                             <div class="single-car-preview">
-                                <img src="https://upn3pw.ch.files.1drv.com/y4mZs5ZDPrZvBAo5LFREH7Z6BpzGeooBGCdK29rbr9E9miZoqgnjjv7JdLnIfbVjQXjtcpDTpAp5efgvazmifq-HpX_RgmPP-8OGsTNEEfiMw412yMm9TI_hquxNPR7akG12MMi_LgmUlG2rj-_7kgaxQ1W8I3NbvThnlzbX7qfl_NN7pFNM4_w9GzAiWmabruTQirlBEOOc7jo3cjLcA1zRQ?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhrjbq.ch.files.1drv.com/y4me2vRmHgbwOjKyrwk-FRZ6wx0dt-AUil7JLmPbBOdYcQdzDesQkQFME_HsyJxykvloCZK5ZecftKv-KxfGBzeDF3hq2T7stgLTTwQdeQq70CsRiIj-fRzfUzvMdmpdgYCT6EAkj95LpLa49Hjb3hJFuZQbb65dWDK3-4NrKlK1LBrg6Fo-y8sF3ZTri4SWiY1bnFON_JeR1s8JgK7UPn64g?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://upnbvg.ch.files.1drv.com/y4mxbWVnksT_g7K6zqfrPXPYauLA3Xbc2hfSjLo4a1m2tQQHnfSNLEusOKiJ1gHOAnRW4WpkUOeuqat73V8HSaygZRFBuMw00SpT8AFujyacG7W2CrBbg1elsM6ePogZSZixC8KW12yMkbh-WzxLcoGieENPV3VTTGWvGu3ctghmc7BHbqdsJTsqCGVp2Ql3nTpAjdKg3sXgL8WCCuRLODCag?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uplceg.ch.files.1drv.com/y4mtVVcWcTKAC8pRwCfg7Ntt8MdZnrPXlg7ONESB2KYS3EqKt01hA_Ad6HX3wSZybZLyIZiNhb4tyUPGWgMIsE7UYnmIP9RLAuXGbQUf-SJlmWxZFNS15C2MeZBuWOABwEA4E8vhjF2LeGl7NPrBxhQIARoybJDdWQNfbzHmz4dc7836UGm8l-bETusbbuT0k1FtuudTUNF1647zAzHjGiurA?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://wjdkuw.ch.files.1drv.com/y4mkdPWb5Wik39sstXe_WnxELEhUDH3kJmCwZAa-72RRNVKAg_F33txfEZakh2lYxW2WUmmo7985G1e157sgLjusZFytMsWIyCDb1RiXv2mYr6SGJdbTz13zjhKEhsR5HPj3z6y42bfpP8Db3O3BnTV_dM8GMetcuefzg6yFS5CErrrIJ5wC6QvuFotlkOuAL9dxm4x1aDbkCBi_jJwStUJhA?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://upnigq.ch.files.1drv.com/y4mGxsZcIrzQKYrpNqlDQ42A0X5wStwuJA6ON7MQsGclW6FW-GQbjPi-7K0RYycJkFTZp5zCfdDLkL1D13LmnpsAkwHqJP0O23czSDXj4ntbBapVR8jQucJyXojrbVjKELqsMjkJT-PNEDkRTW-Mer6-2hC2mj8g-_xr0q9MRO0AMzTt8-3MwW9DVXo_AkmInZZ2_i79kjf0xKxfyemqqR3uA?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://upmsha.ch.files.1drv.com/y4mxMzyTxS7cMSUuXNL_BOEvUE_oyt7c-KZiEKVEWKmdLqRFONZA90wziwwn1DSM8MopTwV702MgbK9ZRKTieBimt7pKti8b5MYQV4Sog9yxi7i3QWUoLYoYBDD-6IdxBJQa-9dII6HBvh5rp56JYawry6UAIsEijccJvNnCZaGwq1ALJWUGrxXKkc3FaJvm1Tm62fxwZlHsSM1zId5UGIG4Q?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://upn37g.ch.files.1drv.com/y4mN-FRAPdEkR__GmdQB_qmiZAK2lPSOCF-AC1mntaK938eLpztny9I6CmKqpVmUoTwEbEpumxBGIxKKB3UEc0x9IW6sLUTI9pvAHgt6Op64TnuNDZEE3jm-KHRKYVONHKosUo3blqAEQ8p4cECVBwAQY1AY7CcLuzadVU8lPSZMJNYU_aNDngItOIDb2wy9BGCIGuyweyCn9wvlVsPUoWyig?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://upktkq.ch.files.1drv.com/y4mCOIBZYucLMk7wj_L1M__-BiaR7RG7VeOd-YJx1AP8-TNV7CMU0GDhA9Y0NP7OojGoegRd6o3ua6xIsIYtirfILqZISeWb-kqsdWWchSTk8QvCrXNrh6O0sfsWrpxKnfn2QuUh5gXPU54PYy-d2ynjqKurKeWQkWFRf08EDq200txcxTC0ObgoT8f_eKVdOR22XKpiVSFX2MYpa3nenLmLw?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://upl9jg.ch.files.1drv.com/y4mfk4WXbaIoJNBh66JaQfWsOvkyscukaDgyyJDt4ECB4KRaC7FmptJ9ozbQot4j_3ZA0Y_UwA9ijaLpvHlyEkk859Pp4fvcGz8N7vk-XIOjmiQsqAnCN1e9BjkOpTKE2UxPUVsdy_BKFsJQJ8SLUALdFPoa7HQqzCFxyz3ddRh3uxBwhtaZpuxdKckf9T7qDMIZaWNSgEh15oQHf_Pe0NREQ?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://upnibq.ch.files.1drv.com/y4mm_z_vbFY872SmWZlwFvHbqKA3imA2l9b5iTSaDFhvqEVSSt9KlwzSZHJvjdJbeqsuII2-RRuhaznXAB3Mk4PBQ8RQPaisTEOvcImCsPfgmq3_o9Eqcw6dnmMrNVME2WViqIuFJswxHvoV4GaYk2YpVeyrlApMd3Cc8sYTeXR-P9XM2Xh87D-1YKR6ikS4ZFECwMnH0lACYzCgYsLGd2vwg?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhqtha.ch.files.1drv.com/y4mETrUP3x7ef5UAQVHApGDv6GMoLbn24OiTOlPCTUSVJDueIF-X5gr7a7dOYMZPs9p29H_wbTvaPKEqAXW5bKujhPVambf4ohQ5xvpC51yZA2_foZs8aKNkjskxeUUDK5tfbEFvZRfcISeHcTohx3vdIVInh06s-2R78SVrfRhB6vvnk3xAe12oQNUv9QWLiNdXDj0hjWSZCRaS8k9yFHjtg?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://upljeg.ch.files.1drv.com/y4mEHSEMDA762GJ9JXVOQkAgYnZ5IXRsD4s7C_J3renhcM01cYNKaNs0guepWVOa8thNc0fSmNt2VZzLcH2wIOGR_JpU-huud7rF4w-h1q6PVqCJrS1tmteXu0QTdCY7HinJGmoceOU9fKYW3sdLfuLeUohmCE0u36oN7-7vU5yJsWuO8e5H32rvvlFJVWUx7b8tRyicMv_PsyuYFbdtiU1FA?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhsuka.ch.files.1drv.com/y4mzGxI42dLCCPFNS6R810r9P3wtkWc94rdcSn3gZ7wks7GtDsGJHgeZyBcansHlDkt6dvpUjH1Ct0ygIoJaHXFMg1pscuJbdOLh195e3Ko3IU7ySevaYwkkMeKJfqDhtpb3BJbC4NPf3saBVXu4TRGppGuaAdjyygwnS7LXu4CBpOdN5xyyOWj09PEiH3oemDOw6S35jkbvszcMw8K5udzNg?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://upnpgq.ch.files.1drv.com/y4mk4K7YE-DmuZ0Zq7sgX_McE1Bfv2Wb9GQt_cx-B49UCOdUyLBNKxPAIzfnG1wQrHyQ91_jWwqpuMQYBT80NzFolK8dMQzKbn5H5hLU6XS8PTy2wTU56cS-MfIq-m7dzsyY-55zq10E5vjszNF7vhCnWdixWDDuoNigUKi-IjhxjA2lyNbWmXq-mKhVJD9TIN_HxVvpqurt9i8y0SejVGsVg?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhtkeq.ch.files.1drv.com/y4mIpB9qHsQiFjr8wDqqObqYaOoe4kFw5XdDkl5kX1wLIvnVnK7W_B5zSGtrRWxNd4An5D1QF_CJDQN2-lbn1gCrCebg4sjkk88agFCQwva6gIhp4cNtP-fPwd6HN3yZeG5Mttt3D0K1Bl19dGNY5KgyGammiSYLz87CNuFN74KlEFbSZhAKbnYB9gYQy3UeXO0E95uqR75u1feeu0DVCJW3w?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://uhqaha.ch.files.1drv.com/y4mPdT4JBc3iAVetf5YMOuwkfBwn_QRTKBlLajZSSzIi5GkHqnxPgDXlLMKoo0NcRGr0nCYU12VkZdW3poyGbxj_JPrhaFR7nKR_dYKqVjKuR3rAeuWwb-Z66NqCwPTibEssGoIYNt4EfgXzxvNPNdsq4IlR4D-OBQNpDzZqLbh4cxjWcWkuBuqN80dXz9jDB2nDT8yohRGRBuXNflL36rGBw?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhqayw.ch.files.1drv.com/y4mDq-DJVFsDLCsfXnl1nh63Bol4gwh4Vo1EJvRD8OAjdg3uKvrn_EgcCRPBU9D-Iu-T1K5IvLKxQkNmeQnjGkftH2YYOE2DKCYXZfTXHPZVnXzu79478d533jmLFhqGP8Cm5fzUdk4TnQC_N7QNl8jyTfxZ0wieAk9vGfhAhBdNmXZA63aF0ZT42SapMd8pCWvZQ1jnQSWr03dkQSOLpDjBA?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://uhs1ka.ch.files.1drv.com/y4mISmNyAxT7mAVKSGW9bR--fFk9EGJsx1xSdJIByY7b9uWAWiyVyi_IoTkZ2yxXwhz3L5SIdoXvd3XIIAifkQ6PSIJrf75kQgjrXrBk3wbWshdx14cH_DKmXAgBSRvkwgdyav8tQ_pVY2KWF3JGl3H7eNk9QK2YaWr2io_VZoq4sf7dU8Bkgg6EP6rWJg2ZRzLEi9DLgP_fpQqYJi7ooEJeA?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhrq7g.ch.files.1drv.com/y4mlQnWb1TPl6cAFyZuHAU54tCmwtk2CoJxxhFoFx5i7xevJeaCZCUMxMK-XvLgcGAHo6lrJ1g_ea15ctuCUZfPgD-RSmAi7iqwAHwFpNFfbPxCMCUy36EOW6sAPTTAyJE_BIrhRirErvxMxfut4TxH5DGiLL3DGm3S-M-jmrPnVlsoJu-FjbixUa4L-EZVLrv5--5wBAy09jvMwcnB828dpQ?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://uhrqbq.ch.files.1drv.com/y4mh3rvKpegpTuwgfpMUo8dxo1o8Lspu96O9DuwIDr84kBp8LYZdfa8hGwZfCpRPh4LSwflAn7oe7aPLFOrryL2egtK8HgfyzpbEOt-R1h3CLoK1tVtDfTh9IAxpIieJWGwrl8harR2DkP372NlqdnhWJUf4T0QM1qsrn912TVeZJx-1vgsCZDhDAstWgNTml_-gyiU6R6bZ-k3DRK9qt0cxA?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhs11w.ch.files.1drv.com/y4mEFjxizL3OPCeNRt3wHcZs9rhGRSAZZ2DcKLfdfvKbOKLnSJJg-OIzXBVECNaNSPPFn8wOkQep8P9YtbeLxO-whavx3PUoawuHSZpfs4ONd4gk1-FdvTLXOXE2jBQ40Oe-A6d9L83Pn1xQd4tTBOM0lBYJ_a95AS6vdKTctsWq5Vak7YJ6e5AbefE7ofQ6xUmWewEOGYee-7Z7t1Zr22EKA?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://uhtreq.ch.files.1drv.com/y4m7jO8cfQnyoqwR_eD-uv5zC8qhAo3lktR5WJ1ENtjBDMKwxU5mhbH9s4_cGxm5k6hwnLnCWCT_jfA2cIFoxVKIBw9uxNMRrKZAlU-xPU6XQCme4oxyNBAlW_wq7YkOtFT60g0NqaF6Kegkl1kVMD_MygEOJCPQ5v9U107NF6dwkNAj_Zgiau-LIp5RBad35avDAHKCzLXfnJlqVFqORS53A?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhrqta.ch.files.1drv.com/y4m0bpaMz024EubSHivumtJq2PK6LA5JT7yZK6Mh7o5nxWBiJqVc-U42GlwYeU3xT6X2mFPeRLtrtCFxNmTjI3EuELPsLhwBKGzw6hYdrq9xP4brTbrLEG9iz-Dj291hH6tZw2Pv4JhzSakXaJfYlx2_u_aIVwKvxXKEMCeEFtFv5748PqP2Y0MMWFE0ONBai6lHMXax0tdvFQPqh731ySF3w?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://uhs81w.ch.files.1drv.com/y4muaaZlb1ghssC41wtYfuqBGOLhREp68lhSlfMuUhJOZJX--S0RJuSSMkU8P66ADi2LELHqLeW14On7N0MHMt58uQngYLfjAyVDnqpLtM7H27NbZ2SiSn3f_4my_x3pSThuZMfMG9PoFCUI_ON2YCkL-EaDRbketQpoKMZVQcaGVFOIZ4oxMq2laKqtgQRw_pN3PczkOo8aNGKFN5PIYJmXg?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhq7dw.ch.files.1drv.com/y4m0_DorVLicxxMtcbnhZ-HtxjQzSAWUQl0IJB_ZfALbXhbX3l_b4ZgATE63hKHXwZ5EGedfsFRT7Lb3LEKQeNoqCLoSvGN655IiScPT3HpD-oMh69CepFrjSwIVXWg4zgKDJt1FuitdFqbWv9K8zInd5WzRriQgIDf11cfZ8Krq_CSrKS0a5hDWA7XC07QYRrgwPEP3rBuEr-csPDSisC5Zg?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://uhrcdw.ch.files.1drv.com/y4mlo4fADWOw81A-5GxZMYid8_ocBlQqh6h6xboh8TPBSrWUi3azBha-wjxvgCtL-9bkQlZ_8MwoRp2usk7qQGmz02ikAJ0YEteBE4cOphiCgzBuY6JN6MF1PjkGZBXOsB4jDyzkeQs-fpvEgVcEZSQZVrMBNc_GwNvHuoGjwpLmRe9UgGo5Onj0DO_0EnkBceNhiC7t2SOwMI0MHSKVAvnVg?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://uhtw6w.ch.files.1drv.com/y4mELWxu-qxw8eyKG2fxU3l3KPpTBaLNAXFWROMAPhS_fhT5k89TSr9ghObddronRn6SnOj-9h_vSQfBrn7IWFGUH30p2z4qy6G-CeVJR6Ibx106o1lh75GnkqCqim7oB1Mt_FUZ6FFsAT1LZ3R06CBncsGRaR0x2QoMOMjHG0X62XbtBA7PPbuCWo3Q_VDXjvljqTF2qchmFs_6yxKiW46vQ?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://uhrxta.ch.files.1drv.com/y4mFrsAKxtRk94k_yYZhl1rdEJUm9mOQ9pQGZJh-hBk0kjhS43mb-pKZxFjo6kXXMcu073wdSXY47RFDDsK0YxNi9i4caCDt6UCdRqufU8RV9tVrx0WFVMxp47nJ36kbzv7Yqjs9ou9YswjzreZEtT8f_f6RHS_6_Rcrw_AMjYb2Rckf6YAcbjeEp-NuSe2CAfOj_I99c7-c1VHHVVBnZOfbQ?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://idijba.ch.files.1drv.com/y4m1HxJKRXKXv3jeCMwIAqfDvu1M78o6CX7xiIwBJt3p2U3SdISdL0JOfuVt47RK23QVOcZdCx-NPDNex9dsA88L9aEyKzpn9r4vG8a604q6DNS8BhXL52GPygFuM_AWuM81LFpwJyEptA_Je7Fxxcu29bOy1NdXbPiG-9OVDz5h0pTCE3j6W9OSNbEbAJoZ4ybptsAHfqXaMZMxoN8FOBTkQ?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://uhtd6w.ch.files.1drv.com/y4mlNGy5EsWtHfdHya0KD9hFNynH-TiBlWFBMGpFzXT3haMsaCBnEaLiBZoZCuunhkDWRtPdlnGVtBgOHnb-3lc9iK0e3rkcO6hzkqSMNu5Fl_rZVDQJa_T8kAdjoH6Zt_z47do2ZvqcgyCacJzYOw12QmyLjTO9uU1RN_91zk35VxUbs0VeQOEJZM3_KF_GNe8ElwtdK1OLxtSKrGEz_sWVw?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://idkkea.ch.files.1drv.com/y4mMPKqEaPp9iZrtryihdFBeig7CzYzT3HGFDPddaonBF7gRan9LHoZPa2sASeNoJBA8Dfn0jChlMyMYDQN0CS1aQxDPDdcoZFWR7FAC0v7mPIiEXjtZgubUvz6J9vmXaYzoD2ZbmtmeVvgHSo6f-QU93BJiR2qRWTw46kBeDIizUYXs9j-hO2ZFtX-PfcLmvp5EipnE3h8WyxDYQNXeLUeKA?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://idkrea.ch.files.1drv.com/y4mgBF_rv9Sdi6rWxihUGBQ5zBzPx2VcHWLEZZfK0VYT0BGBp4-ZZCy2CzK98ce4RHi9WlWeNKsGg8o3NAe2NPO2aYObPGFVoN_yhGTV0FuXIGV8R03TwR1NloD91GqcBeV6zRXw9pxbTheSgVM3PskiyvG-SQFs0GUyDWvSXX4VmG1s7LDoAGFOvS1qY-1H1ORGAoEuRCvCQItmjOHkCVMHA?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://idiq7q.ch.files.1drv.com/y4muWyBl8vqm-bgZGm71jXuiu2FI1DDHZe-E6qPhNTKsJ7nVBAasj7PmDMr5xPgPPw_6UP7n_qk7FOb2wBfgOz3xncFy6yQSpLEDzs2mZfirbYmqVymtc47RW-JnwFKsQYvSoTuY6TDI1xwaCatJa1ogoSZ30O3EIAPOQdnB-WdTgYtdcXl9E009KVstq1iJlTsiCAZsCs1jlpmwbfwoY7ntQ?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://idjg7q.ch.files.1drv.com/y4maqkuMmon30dTvr0fqWhVKHOVZBJUnSlGxqH4yRAxHLvPnlgbt8_EW0WXUMRXHaNgig65LoyPBRPw-P7acNCiVbq44OuXlvg1VbK1jUPpaSyU-sndv6pRlmpQmguLFXlFAokH7Yrxu_ntig6ryyZCm8MgFAFKv5nz5v1gjU_lu2AOOKDKTSZ3I3SMUiIh1VcMfchcstGoQt0cwIohAB_gGA?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://idlayq.ch.files.1drv.com/y4m7mXtemV-ULnRvMCb5QBHEW7uyo4-fW5QWGbl_ckti4-5buhzEjbb7OxMK3n16uAuOLoBE1mEIeuQHB4vsj6mynjEp-3G-NlIXRcUfepT7k5hMCm2Af6V-IgAdlnav1A2lQ1RO6XZJwQqfiPkgOkVx3EJTbrJNaQzw2zFehXnThYCNmbGexO8nbl-KfLtAt5yVsCCPrfQ4rHXL3ofPP75bw?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                             <div class="single-car-preview">
-                                <img src="https://idlhyq.ch.files.1drv.com/y4m_K4y11pw56OqiQFC_2FRen8N3g8PgaKNQQpbtFyl3hPhPkbdvNXvbt8qhfewi8zpm4FDNDNqi48CsF2E9JnU90sjvw1oB0BOJGU3FXqyfRHHdX49HJRh0eI8NXSyBq0cvTiK6vWyNaq-nKN8KAWUT_JahPgpOt26wD5FJmyuMEGsgMaZsO1C6oVY6GRklT7g5Ngdubh4oK0qFaKT2eHG1Q?width=4608&height=3072&cropmode=none" alt="JSOFT">
+                                <img src="https://idj11g.ch.files.1drv.com/y4mH10u0XEmr-o1nHSSlCiAqzUOAnMS8kC55G1PzD8S8B-RYA_s2CpBOLo-gbXTqArtaPBcs9i5am5Jj0l1kvB4uF09GRaiitIG79T6i5hnGPpGWNVfniF_RKQk7uCCthc6wrSNZBKEtQEIz7WTU8V44ZUohlbfDjsRuFRX17xz-NyT0uObFoYKtsr7g4y12frTB3s81eJY7lSYcdBod-T_pQ?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://idiqsw.ch.files.1drv.com/y4m45IxpQGxDRm1EnlS2cCKr12E5BEUZLZCmuyxyESIPI0bOWgt84ox7mkfdz4LZZ_Ipf9Vs6yTSmkD6wcfTUYZR8vO81OkVNTQSphfyn9zmnpIaD7JIEgfz46rqu4fSrRodAdk88tsk3gmaiyB8mXW6JgV8m0HwIM-JkF0UciokeRQitVZtYePO8FzfNKUM5OAh2q-IJXF9FZeLkm4Dq8moA?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://idkrvw.ch.files.1drv.com/y4mNzmpFbdlVRMW0ppPAlOEvd630Onw9RT7blI6uWVEve5fkohKB9f7TasDKAqUW5VIiqjX_Nxkrmtd7Jjwazu3t5brWWXt2azQ-Ff7yw44czueqyqbD2zEGQOkL1yDEKqJN_35MJftON_bfohKqpCHjVoJ7ltFV6lfFBF-WuWy-pexxMtUJb223R19N6zsE2ioN-4xBj_2kT9ycvHriKQB5w?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://idjgna.ch.files.1drv.com/y4mMXrGJO7WLeOafYUEiIGYE_owtzzoRCTQlPLI_jw4IoYw-A2h1eMoUVvGqRzhbqiwUgCoUVlc5c6PwTJf2mmcDQC6XlW_vjkHKX1lZ3T8kgkOub1a5K7BWRDRxjBPXeP9D4dz_vcjxvl7zDE8zKH349VLorhe-s7YO4-NKkrWj5kpMHpHzGq_7xO0nIFiaaXcVRfwZ55r9zoYsTIrmsdT1Q?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://idixxw.ch.files.1drv.com/y4m2GLhZ60qlYkif0ZL7_NZCgagDR8oXfkivgTRei3KfYLjgYdSIhr0mtXV4gULdZ6SOgGV9riJIAw2I5qXXczzpgY5K3giNIwouT135Ty4oyVoEzpQf9ComIMSdfxRqGwxLIJThFyHZ46yaMfIzEzj04EM8eA40_VclrJ_6ewJ_wPnzuosd7HkicZp9UjieMx8cwNdQriId4FFjiEg4mQ3QQ?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://idlm0w.ch.files.1drv.com/y4mqCkQQNJ0FY1N_MOQ3OCIkDFiCuzSKia5c_Yb7NhEco2E_wfuM7DweIj3sV6v4RR8pFr0_blGuQpFF10DkUplrr-msYGacSRts-DbID3PgMd2alO9tFZUNpCetoiViXf3R8Vj6jVfBK7Sp4TofH5YXGg0VoIwQ25iTawKQcEPszDiWIZM2OE_6iyMfg3v2CH2x-3x6SDWi4dVRKycfafREw?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://7l1iyq.ch.files.1drv.com/y4mhfF-j0DfO53ZSMLc3Smu3VgQrbkUy797V-TJofvWHESaacT95l4wOrUzI-n5wRa723Tbe2x5qBqz0phKdH459IjFW7jX6d1U0WKz0z4S-jpuH-bti4LPm0pIEl3ZHb21n7_XiwU87CwJWbVMNtfz1_L8Dn244sqtXCNYkETRXVYmYcdA48OND1gNIv8U4QcuXMMR7ectjS4uaFGIBa2z1g?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
+                            </div>
+                            <div class="single-car-preview">
+                                <img src="https://7l391q.ch.files.1drv.com/y4m8DpgpMJsJdSbquvtjK92rD6IpGpD3qLYwF2tcRZ879aQfpGgOVmTN6JyJiKE4wKSuVJ60zmN7pjcOPooRGRceng_gQidJh3odx0kVVzubJSqSdvamDAH_WNnsVDdhP_-d36t_NbOOjDClXfm0yTE4IR9fIIybxczVqFM6DIspiC0fsP692bYgKmQjZgz3HqtGDC62uf_PNA0rCOj3NDb-g?width=4608&height=3072&cropmode=none" alt="Rolls-Royce Phantom Drophead"/>
                             </div>
                         </div>
                         <div class="car-details-info">
